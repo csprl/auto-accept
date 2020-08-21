ef582e9bec06d5ab7a55b0b5991f4184033f4622
Casper Löfgren <casper.lofgren@hotmail.com>
2020-08-21 23:11:39 +0300

    Main application logic

--- AutoAccept/Program.cs
+++ AutoAccept/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoAccept.Helpers;
+using AutoAccept.Models;
 
 namespace AutoAccept
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting auto-accept...");
+
+            // Resolve game path and info
+            LeagueClientInfo info = null;
+            while (info == null)
+            {
+                try
+                {
+                    var gamePath = LeagueTools.LocateGame();
+                    info = LeagueTools.GetClientInfo(gamePath);
+
+                    Console.WriteLine($"Found game at {gamePath}");
+                    Console.WriteLine($"Detected LeagueClient running as {info.ProcessId}.");
+                }
+                catch (FileNotFoundException)
+                {
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Failed to locate game: " + ex.Message);
+                    return;
+                }
+
+                Thread.Sleep(1000);
+            }
+
+            // Create LCUClient
+            var client = new LCUClient(info.AppPort, info.AppPassword);
+            client.OnConnected += () => Console.WriteLine("Connected to LCU!");
+
+            // Register ready check callback
+            client.OnReadyCheck += async () =>
+            {
+                // Wait for a short while before accepting
+                await Task.Delay(100);
+                await client.AcceptReadyCheck();
+            };
+
+            client.OnDisconnected += () => Console.WriteLine("Disconnected!?");
+
+            // Connect to websocket
+            await client.Connect();
         }
     }
 }
