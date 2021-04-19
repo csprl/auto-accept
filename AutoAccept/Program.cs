5db4c15fe902821e53bddfa6972a9bc70930182d
Casper Löfgren <casper.lofgren@hotmail.com>
2021-04-19 22:53:30 +0300

    Prevent double-starts

--- AutoAccept/Program.cs
+++ AutoAccept/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace AutoAccept
@@ -8,6 +10,14 @@ namespace AutoAccept
         [STAThread]
         static void Main()
         {
+            // Prevent double-starts
+            var currentProcess = Process.GetCurrentProcess();
+            if (Process.GetProcessesByName(currentProcess.ProcessName).Any(p => p.Id != currentProcess.Id))
+            {
+                MessageBox.Show("AutoAccept already seems to be running.\nCheck the notification area in your taskbar.", "Error");
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
